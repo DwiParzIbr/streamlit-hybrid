196503793a08b0bacda0183cdad97807b9935e4c
Dwifi Parizza Ibrahim <99091149+DwiParzIbr@users.noreply.github.com>
2026-03-06 12:22:42 +0700

    Update constants.py

--- constants.py
+++ constants.py
@@ -9,7 +9,7 @@ DEFAULT_MESSAGE = (
     "sequence includes alphanumeric characters, standard punctuation "
     "marks (!@#$%^&*<>?), and extended UTF-8 characters (é, ç, ñ)."
 )
-DEFAULT_TARGET_BIT_SIZE = 512
+DEFAULT_TARGET_BIT_SIZE = 4096
 
 # --- Method Names ---
 METHOD_LSB = "Spatial - LSB"
